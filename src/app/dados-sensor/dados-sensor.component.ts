4d398d60b5d5d92693e3a628ef73d747327aaea6
Caio Viterbo <caioguih03@gmail.com>
2024-12-12 18:38:19 -0300

    Removido importações desnecessarias

--- src/app/dados-sensor/dados-sensor.component.ts
+++ src/app/dados-sensor/dados-sensor.component.ts
@@ -1,7 +1,5 @@
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { FirestoreService } from './../services/firestore.service';
+
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,  } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 
@@ -15,15 +13,11 @@ export class DadosSensorComponent implements OnInit {
   displayColumns: string[] = ['id', 'nome', 'valor', 'unidade'];
   dataSource!: MatTableDataSource<any>;
 
-  constructor(private firestoreService: AngularFirestore) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.dados$ = this.firestoreService.collection('dadosSensor').valueChanges();
-    this.dados$.subscribe(dados => {
-      this.dataSource = new MatTableDataSource(dados);
-      console.log('Dados recebidos: ' +dados);
-    })
+
 
   }
 }
